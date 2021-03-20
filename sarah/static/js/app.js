2da0f15392e65ef9ed1574d5281d45d8b71823df
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-03-20 15:49:28 -0400

    first go at chart creation

--- sarah/static/js/app.js
+++ sarah/static/js/app.js
@@ -1,29 +1,67 @@
 var path = 'static/data/data_cleaned.csv'
 
 d3.csv(path).then(function(data) {
-    data.forEach(function(datum) {
-        console.log(datum.genre);
-    })
     
-    // var trace1 = {
-    //     x: [1, 2, 3, 4],
-    //     y: [10, 11, 12, 13],
-    //     text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-    //     mode: 'markers',
-    //     marker: {
-    //       color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-    //       size: [40, 60, 80, 100]
-    //     }
-    //   };
-      
-    //   var data = [trace1];
-      
-    //   var layout = {
-    //     title: 'Bubble Chart Hover Text',
-    //     showlegend: false,
-    //     height: 600,
-    //     width: 600
-    //   };
-      
-    //   Plotly.newPlot('myDiv', data, layout);
+    data.forEach(function(d) {
+        d.genre_num = +d.genre_num;
+        d.bpm = d.bpm;
+        d.nrgy = +d.nrgy;
+        d.dnce = +d.dnce;
+        d.dB = +d.dB;
+        d.live = +d.live;
+        d.val = +d.val;
+        d.dur = +d.dur;
+        d.acous = +d.acous;
+        d.spch = +d.spch;
+        d.pop = +d.pop;
+    });
+
+    var data_year_filter = data.filter(function(d) {
+        return d.year == 2010;
+    });
+    
+    data_year_filter.forEach(function(datum) {
+        console.log(datum.pop);
+    });
+
+    var genre_nums = data_year_filter.map(d => d.genre_num);
+    var genre_name = data_year_filter.map(d => d.genre);
+    var title = data_year_filter.map(d => d.title);
+    var pop = data_year_filter.map(d => d.pop);
+    var decibels = data_year_filter.map(d => d.dB);
+    var decibels_manip = decibels.map(d => d + Math.random)
+    var valence = data_year_filter.map(d => d.val);
+    
+    var traceA = {
+        type: "scatter",
+        mode: "markers",
+        x: decibels,
+        y: valence,
+        text: data_year_filter.map(function(d) {
+            return `Track Name: ${d.title}<br>Track Genre: ${d.genre}`
+        }),
+        marker: {
+          color: genre_nums,
+          colorscale: 'Rainbow',
+          cmin: d3.min(genre_nums),
+          cmax: d3.max(genre_nums),
+          size: pop,
+          sizeref: 2,
+          sizemode: 'radius'
+        }
+    };
+       
+      var data = [traceA];
+       
+      var layout = {
+        title: "Decibels vs. Valance in 2010",
+        xaxis: {
+            title: 'decibels'
+        },
+        yaxis: {
+            title: 'valence'
+        }
+      };
+       
+      Plotly.newPlot('bubble', data, layout);
 });
