521abf3ad05af8a7d100537121effc3878775c67
Angeli Lucila <Angeli@Angelis-MBP.home>
2021-03-23 21:55:41 -0400

    m

--- angeli/static/css/app.js
+++ angeli/static/css/app.js
@@ -87,7 +87,7 @@ var Yvalue = dance;
 //   Plotly.newPlot('dance', data, layout);
 
 
-  var xValue = year;
+  var xValue = ['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019'];
 
   var yValue = dance;
   
