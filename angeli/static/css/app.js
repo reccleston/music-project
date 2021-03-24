80fce783d73975f55d501479692f871beed0ef42
Angeli Lucila <Angeli@Angelis-MBP.home>
2021-03-23 21:15:58 -0400

    ne

--- angeli/static/css/app.js
+++ angeli/static/css/app.js
@@ -16,11 +16,9 @@ d3.csv(path).then(function(data) {
 
 });
 
-
 // Create empty arrays to store the dish and spice values
 var year = [];
 var dance = [];
-
 
 // Iterate through each recipe object
 data.forEach((x) => {
@@ -40,49 +38,84 @@ data.forEach((x) => {
      });
   });
   
+var Xvalue = year;
+var Yvalue = dance;
 
-var data = [
-    {
-      x: year,
-      y: dance,
-      type: 'bar'
-    }
-  ];
+// var trace1 = {
+//   x: ['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019'],
+//   // x: year,
+//   y: dance,
+//   type: 'bar'
+// };
+
+// var data = [trace1];
+
+//   var layout = {
+//     title: {
+//       text:'Danceability By Year',
+//       font: {
+//         family: 'Courier New, monospace',
+//         size: 24
+//       },
+//       xref: 'paper',
+//       x: 0.02,
+//     },
+//     xaxis: {
+//       title: {
+//         text: 'Year',
+//         font: {
+//           family: 'Courier New, monospace',
+//           size: 18
+//           ,
+//           color: '#7f7f7f'
+//         }
+//       },
+//       tickangle: -45
+//     },
+//     yaxis: {
+//       title: {
+//         text: 'Daceability Level',
+//         font: {
+//           family: 'Courier New, monospace',
+//           size: 18,
+//           color: '#7f7f7f'
+//         }
+//       }
+//     }
+//   };
   
-  var layout = {
-    title: {
-      text:'Danceability By Year',
-      font: {
-        family: 'Courier New, monospace',
-        size: 24
-      },
-      xref: 'paper',
-      x: 0.05,
-    },
-    xaxis: {
-      title: {
-        text: 'Year',
-        font: {
-          family: 'Courier New, monospace',
-          size: 18,
-          color: '#7f7f7f'
-        }
-      },
-    },
-    yaxis: {
-      title: {
-        text: 'Daceability Level',
-        font: {
-          family: 'Courier New, monospace',
-          size: 18,
-          color: '#7f7f7f'
-        }
+//   Plotly.newPlot('dance', data, layout);
+
+
+  var xValue = year;
+
+  var yValue = dance;
+  
+  var trace1 = {
+    x: xValue,
+    y: yValue,
+    type: 'bar',
+    text: yValue.map(String),
+    textposition: 'auto',
+    hoverinfo: 'none',
+    marker: {
+      color: 'rgb(158,202,225)',
+      opacity: 0.6,
+      line: {
+        color: 'rgb(8,48,107)',
+        width: 1.5
       }
     }
   };
   
-  Plotly.newPlot('dance', data, layout);
+  var data = [trace1];
   
+  var layout = {
+    title: 'Danceability by Year',
+    barmode: 'stack'
+  };
+  
+  Plotly.newPlot('dance', data, layout);
 
 });
 
