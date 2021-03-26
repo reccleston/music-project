c7b259ce322ab4e80ba88de7c49611d4565b17e5
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-03-25 22:42:42 -0400

    merge resolve

--- angeli/static/css/app.js
+++ angeli/static/css/app.js
@@ -1,89 +1,93 @@
-// Copy of Sarah's app.js
+// path to csv
 var path = '../data/year.csv'
 
-d3.csv(path).then(function(data) {
-    console.log(data)
-
- // changing types
- data.forEach(function(d) {
-    d.year = +d.year;
-    d.dnce = d.dnce;
-    d.nrgy = d.nrgy;
-    d.val = d.val;
-    d.pop = d.pop;
-});
-
-// Create empty arrays to store the dish and spice values
+// Create empty arrays to store the factor values
 var year = [];
 var dance = [];
 var energy = [];
-var val = [];
-var pop = [];
+var valance = [];
+var popularity = [];
 
+// list for the dropdown
+var cols_list = ['dance', 'energy', 'valance', 'popularity'];
+// dictionary to retrive lists from based on dropdown
+var col_dict = {
+  'year': year,
+  'dance': dance,
+  'energy': energy,
+  'valance': valance,
+  'popularity': popularity
+};
+// defining factors for x and y axes
+var xValue, yValue;
 
-// Iterate through each recipe object
-data.forEach((x) => {
+// functions -------------------------------------------------------------------------
+// response when user chooses from the dropdown
+function factorReact() {
+  d3.event.preventDefault();
+  // update yvalue based on dropdown chosen text
+  yValue = d3.select(this).text();  
+  // adding to nav display
+  // select the ids
+  d3.select('#factor-choice-label').style('display', 'inline-block');
+  // select the y-axis element, change style and text
+  var factor_text = d3.select('#factor-choice-text');
+  factor_text.style('display', 'inline-block');
+  factor_text.select('a').text(`${yValue}`);
+};
+// response when user clicks the button
+function triggerChart() {
+  d3.event.preventDefault();
+  // makes choice alert disappear again
+  d3.select('#factor-choice-label').style('display', 'none');
+  d3.select('#factor-choice-text').style('display', 'none');
+  // calling the funct to make the chart
+  buildChart(xValue, yValue);
+};
 
-    // Iterate through each key and value
-    Object.entries(x).forEach(([key, value]) => {
+// event listener for click on button
+d3.select('#trigger-chart').on('click', triggerChart);
+
+// dropdown menu append
+function dropdownFill(cols_list) {
+  // appending to dropdown
+  // defining dropdown variable
+  var factor_dropdown = d3.select('#factor-dropdown').select('.dropdown-menu');
+
+  // getting rid of any existing dropdown items
+  factor_dropdown.text('');
   
-      // Use the key to determine which array to push the value to
-      if (key === "year") {
-        year.push(value); 
-        console.log('year');
-        console.log(value);
-      }
-      else if (key === "dnce") {
-        dance.push(value);
-        console.log('dance');
-        console.log(value);
-      }
-      else if (key === "nrgy") {
-        energy.push(value);
-        console.log('energy');
-        console.log(value);
-      }
-      else if (key === "val") {
-        val.push(value);
-        console.log('val');
-        console.log(value);
-      }
-      else {
-        pop.push(value);
-        console.log('pop');
-        console.log(value);
-      }
-     });
+  // appending factors
+  cols_list.forEach(col => {
+      factor_dropdown.append('a').attr('class', 'dropdown-item')
+          .attr('href', '#').attr('value', `${col}`).text(col).on('click', factorReact);
   });
-  
-  var xValue = year;
-  var yValue = energy;
+};
 
-
-  // Function
-  function varcolor(yValue) {
-  
-    // Change Fill Color 
-  
-    switch(yValue) {
-      case dance:
+// function to choose bar colour based on column
+function varcolor(yValue) {
+  switch(yValue) {
+    case 'dance':
+        return 'rgb(0, 0, 255)';
+    case 'energy':
+        return 'rgb(60, 179, 113)';
+    case 'valance':
+        return 'rgb(255, 165, 0)';
+    case 'popularity':
+        return 'rgb(238, 130, 238)';
+    default:
           return 'rgb(0, 0, 255)';
-      case energy:
-          return 'rgb(60, 179, 113)';
-      case val:
-          return 'rgb(255, 165, 0)';
-      case pop:
-          return 'rgb(238, 130, 238)';
-      default:
-           return 'rgb(0, 0, 255)';
-  
-    }};
-  
+
+}};
+
+// function to build chart
+function buildChart(xValue, yValue) {
   var trace1 = {
-    x: xValue,
-    y: yValue,
+    // xValue and yValue are strings -- plug them into the dict to get the actual lists
+    x: col_dict[xValue],
+    y: col_dict[yValue],
     type: 'bar',
-    text: yValue.map(String),
+    text: col_dict[yValue].map(String),
     textposition: 'auto',
     hoverinfo: 'none',
     marker: {
@@ -96,11 +100,10 @@ data.forEach((x) => {
     }
   };
 
-
   var data = [trace1];
   
   var layout = {
-    title: 'Danceability by Year',
+    title: `${yValue} by year`,
     barmode: 'stack',
     xaxis: {
       tickangle: 0,
@@ -111,48 +114,63 @@ data.forEach((x) => {
   
   Plotly.newPlot('bar', data, layout);
 
+};
+
+// reading in the data and calling initial functs --------------------------------------------------------
+d3.csv(path).then(function(data) {
+    console.log(data)
+
+ // changing types
+ data.forEach(function(d) {
+    d.year = +d.year;
+    d.dnce = d.dnce;
+    d.nrgy = d.nrgy;
+    d.val = d.val;
+    d.pop = d.pop;
+  })
+
+  // Iterate through to extract the data to lists
+  data.forEach((x) => {
+
+    // Iterate through each key and value
+    Object.entries(x).forEach(([key, value]) => {
+  
+      // Use the key to determine which array to push the value to
+      if (key === "year") {
+        year.push(value); 
+        // console.log('year');
+        // console.log(value);
+      }
+      else if (key === "dnce") {
+        dance.push(value);
+        // console.log('dance');
+        // console.log(value);
+      }
+      else if (key === "nrgy") {
+        energy.push(value);
+        // console.log('energy');
+        // console.log(value);
+      }
+      else if (key === "val") {
+        valance.push(value);
+        // console.log('val');
+        // console.log(value);
+      }
+      else {
+        popularity.push(value);
+        // console.log('pop');
+        // console.log(value);
+      }
+     });
+  });
+  // calling for dropdown fill
+  dropdownFill(cols_list);
+  // setting start x and y values
+  xValue = 'year';
+  yValue = 'energy';
+  // building the chart off those values
+  buildChart(xValue, yValue);
 });
 
-// // switch
 
-// // Getting a reference to the button on the page with the id property set to `click-me`
-// var button = d3.select("button");
-
-
-//   var CHART = d3.selectAll("bar").node();
-
-//   // Initialize x and y arrays
-//   var x = [];
-//   var y = [];
-
-//   switch(dataset) {
-//     case "dance":
-//       x = year;
-//       y = dance;
-//       break;
-//     case "energy":
-//       x = year;
-//       y = energy;
-//       break;
-//     case "valence":
-//       x = year;
-//       y = val;
-//       break;
-//     case "pop":
-//       x = year;
-//       y = pop;
-//       break;
-//     default:
-//       x = year;
-//       y = dance;
-//       break;
-//   }
-
-
-//   // Note the extra brackets around 'x' and 'y'
-//   Plotly.restyle(CHART, "x", [x]);
-//   Plotly.restyle(CHART, "y", [y]);
-// }
-
-// init();
 
