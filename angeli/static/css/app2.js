f1dd81acadcee5207db9be51d057a7a9292ceb9b
Angeli Lucila <Angeli@Angelis-MBP.home>
2021-03-25 20:15:52 -0400

    m

--- angeli/static/css/app2.js
+++ angeli/static/css/app2.js
@@ -1,64 +1,42 @@
 // Data Table - taken from class activity 
+
+var path = '../data/data_cleaned.csv'
+
+d3.csv(path).then(function(data) {
+    console.log(data)
+
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the data from data.js
-console.log(data);
-
-// // Step 1: Loop Through `data` and console.log each weather report object
-data.forEach(function(music) {
-  console.log(music);
+// // Step 1: Loop Through `data` and console.log each Object
+data.forEach(function(song) {
+  console.log(song);
 });
 
-// // Step 2:  Use d3 to append one table row `tr` for each weather report object
-// // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-data.forEach(function(music) {
-  console.log(music);
+// // Step 2:  Use d3 to append one table row `tr` for each song object.
+data.forEach(function(append) {
+  console.log(append);
   var row = tbody.append("tr");
 });
 
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-data.forEach(function(music) {
-  console.log(music);
+// // Step 3:  Use `Object.entries` to console.log each value
+data.forEach(function(value) {
+  console.log(value);
   var row = tbody.append("tr");
 
-  Object.entries(weatherReport).forEach(function([key, value]) {
+  Object.entries(data).forEach(function([key, value]) {
     console.log(key, value);
   });
 });
 
 // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-data.forEach(function(music) {
-  console.log(music);
+data.forEach(function(cell) {
+  console.log(cell);
   var row = tbody.append("tr");
 
-  Object.entries(music).forEach(function([key, value]) {
+  Object.entries(data).forEach(function([key, value]) {
     console.log(key, value);
     // Append a cell to the row for each value
-    // in the weather report object
     var cell = row.append("td");
   });
-});
-
-// // Step 5: Use d3 to update each cell's text with
-// // weather report values (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
-
-// // BONUS: Refactor to use Arrow Functions!
-// data.forEach((music) => {
-//   var row = tbody.append("tr");
-//   Object.entries(music).forEach(([key, value]) => {
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+})});
