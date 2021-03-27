50b275144f394ad1a7af3a81e298128558df13ef
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-03-27 18:18:04 -0400

    adding hover

--- wasif/static/js/flip.js
+++ wasif/static/js/flip.js
@@ -7,3 +7,9 @@ $("#box-terms").flip({
     axis: 'y',
     trigger: 'hover'
   });
+
+d3.select('#box-proj-info').on('mouseover', function() {
+  d3.select('#hidden-text').style('display', 'inline-block');
+}).on('mouseout', function() {
+  d3.select('#hidden-text').style('display', 'none');
+});
